485caacadd9b64384a1c37024bd8ba1647d80f6a
Anirudha Chari <anirudhachari27@gmail.com>
2020-03-19 00:15:03 +0530

    added local files

---
+++ javascript/calculater.js
@@ -0,0 +1,12 @@
+function sum(){
+    document.getElementById("sum").innerHTML=parseInt(document.getElementById("op1").value) + parseInt(document.getElementById("op2").value);
+}
+function sub(){
+    document.getElementById("subtract").innerHTML=parseInt(document.getElementById("op3").value) - parseInt(document.getElementById("op4").value);
+}
+function mul(){
+    document.getElementById("multiply").innerHTML=parseInt(document.getElementById("op5").value) * parseInt(document.getElementById("op6").value);
+}
+function divi(){
+    document.getElementById("divide").innerHTML=parseInt(document.getElementById("op7").value) / parseInt(document.getElementById("op8").value);
+}
---
+++ javascript/objects.js
@@ -0,0 +1,15 @@
+var a = 10;
+a = "string";
+//in javascript no data types are defined
+var person = {  //object
+    FirstName   :   "Anirudha",
+    LastName    :   "Chari",
+    education   :   ["10th", "12th"],
+    addresss    :   {
+        pincode :   403502,
+        country :   "India",
+        state   :   "Goa"
+    }
+}
+person.LastName;
+person["LastName"];
